22b511205b3fe3d0a1a0325a1f28d6bdaa966a15
DanielMF89 <139999329+DanielMF89@users.noreply.github.com>
2023-12-15 13:55:59 +0000

    api starwars

--- src/app.py
+++ src/app.py
@@ -2,50 +2,291 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
-from flask import Flask, request, jsonify, url_for
+from flask import Flask, request, jsonify
 from flask_migrate import Migrate
-from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
-#from models import Person
+from models import db, User, Planet, Person, Favorite, Film, Starship, Vehicle, Species
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
+# DATA BASE CONFIG
 db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace(
+        "postgres://", "postgresql://")
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+# DATABASE INITIATE & OTHERS
 MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
 setup_admin(app)
 
-# Handle/serialize errors like a JSON object
+# ERRORS HANLD AND SITEMAP
+
+
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
-# generate sitemap with all your endpoints
+
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+# ENDPOINTS | USERS
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
 
-    return jsonify(response_body), 200
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    return jsonify([user.username for user in users]), 200
 
-# this only runs if `$ python src/app.py` is executed
+
+@app.route('/users/<int:user_id>', methods=['GET'])
+def get_user(user_id):
+    user = User.query.get(user_id)
+    if user is None:
+        raise APIException("User not found", status_code=404)
+    return jsonify(user.serialize()), 200
+
+
+@app.route('/users', methods=['POST'])
+def create_user():
+    body = request.get_json()
+    if body is None:
+        raise APIException("Request body is missing", status_code=400)
+    new_user = User(**body)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({"msg": "User created successfully"}), 201
+
+
+@app.route('/users/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    user = User.query.get(user_id)
+    if user is None:
+        raise APIException("User not found", status_code=404)
+    db.session.delete(user)
+    db.session.commit()
+    return jsonify({"msg": "User deleted successfully"}), 200
+
+# ENDPOINTS | PLANETS
+
+
+@app.route('/planets', methods=['GET'])
+def get_planets():
+    planets = Planet.query.all()
+    return jsonify([planet.name for planet in planets]), 200
+
+
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def get_planet(planet_id):
+    planet = Planet.query.get(planet_id)
+    if planet is None:
+        raise APIException("Planet not found", status_code=404)
+    return jsonify(planet.serialize()), 200
+
+
+@app.route('/planets', methods=['POST'])
+def create_planet():
+    body = request.get_json()
+    if body is None:
+        raise APIException("Request body is missing", status_code=400)
+    new_planet = Planet(**body)
+    db.session.add(new_planet)
+    db.session.commit()
+    return jsonify({"msg": "Planet created successfully"}), 201
+
+
+@app.route('/planets/<int:planet_id>', methods=['DELETE'])
+def delete_planet(planet_id):
+    planet = Planet.query.get(planet_id)
+    if planet is None:
+        raise APIException("Planet not found", status_code=404)
+    db.session.delete(planet)
+    db.session.commit()
+    return jsonify({"msg": "Planet deleted successfully"}), 200
+
+# ENDPOINTS | FILMS
+
+
+@app.route('/films', methods=['GET'])
+def get_films():
+    films = Film.query.all()
+    return jsonify([film.serialize() for film in films]), 200
+
+
+@app.route('/films/<int:film_id>', methods=['GET'])
+def get_film(film_id):
+    film = Film.query.get(film_id)
+    if film is None:
+        raise APIException("Film not found", status_code=404)
+    return jsonify(film.serialize()), 200
+
+
+@app.route('/films', methods=['POST'])
+def create_film():
+    body = request.get_json()
+    if body is None:
+        raise APIException("Request body is missing", status_code=400)
+    new_film = Film(**body)
+    db.session.add(new_film)
+    db.session.commit()
+    return jsonify({"msg": "Film created successfully"}), 201
+
+# ENDPOINTS | PEOPLE
+
+
+@app.route('/people', methods=['GET'])
+def get_people():
+    people = Person.query.all()
+    return jsonify([person.serialize() for person in people]), 200
+
+
+@app.route('/people/<int:people_id>', methods=['GET'])
+def get_person(people_id):
+    person = Person.query.get(people_id)
+    if person is None:
+        raise APIException("Character not found", status_code=404)
+    return jsonify(person.serialize()), 200
+
+
+@app.route('/people', methods=['POST'])
+def create_person():
+    body = request.get_json()
+    if body is None:
+        raise APIException("Request body is missing", status_code=400)
+    new_person = Person(**body)
+    db.session.add(new_person)
+    db.session.commit()
+    return jsonify({"msg": "Character created successfully"}), 201
+
+# ENDPOINTS | STARSHIPS
+
+
+@app.route('/starships', methods=['GET'])
+def get_starships():
+    starships = Starship.query.all()
+    return jsonify([starship.serialize() for starship in starships]), 200
+
+
+@app.route('/starships/<int:starship_id>', methods=['GET'])
+def get_starship(starship_id):
+    starship = Starship.query.get(starship_id)
+    if starship is None:
+        raise APIException("Starship not found", status_code=404)
+    return jsonify(starship.serialize()), 200
+
+
+@app.route('/starships', methods=['POST'])
+def create_starship():
+    body = request.get_json()
+    if body is None:
+        raise APIException("Request body is missing", status_code=400)
+    new_starship = Starship(**body)
+    db.session.add(new_starship)
+    db.session.commit()
+    return jsonify({"msg": "Starship created successfully"}), 201
+
+# ENDPOINTS | VEHICLES
+
+
+@app.route('/vehicles', methods=['GET'])
+def get_vehicles():
+    vehicles = Vehicle.query.all()
+    return jsonify([vehicle.serialize() for vehicle in vehicles]), 200
+
+
+@app.route('/vehicles/<int:vehicle_id>', methods=['GET'])
+def get_vehicle(vehicle_id):
+    vehicle = Vehicle.query.get(vehicle_id)
+    if vehicle is None:
+        raise APIException("Vehicle not found", status_code=404)
+    return jsonify(vehicle.serialize()), 200
+
+
+@app.route('/vehicles', methods=['POST'])
+def create_vehicle():
+    body = request.get_json()
+    if body is None:
+        raise APIException("Request body is missing", status_code=400)
+    new_vehicle = Vehicle(**body)
+    db.session.add(new_vehicle)
+    db.session.commit()
+    return jsonify({"msg": "Vehicle created successfully"}), 201
+
+# ENDPOINTS | SPECIES
+
+
+@app.route('/species', methods=['GET'])
+def get_species():
+    species = Species.query.all()
+    return jsonify([specie.serialize() for specie in species]), 200
+
+
+@app.route('/species/<int:specie_id>', methods=['GET'])
+def get_specie(specie_id):
+    specie = Species.query.get(specie_id)
+    if specie is None:
+        raise APIException("Specie not found", status_code=404)
+    return jsonify(specie.serialize()), 200
+
+
+@app.route('/species', methods=['POST'])
+def create_specie():
+    body = request.get_json()
+    if body is None:
+        raise APIException("Request body is missing", status_code=400)
+    new_specie = Species(**body)
+    db.session.add(new_specie)
+    db.session.commit()
+    return jsonify({"msg": "Specie created successfully"}), 201
+
+
+
+# ENDPOINTS | FAVORITES
+
+
+@app.route('/favorites/<int:user_id>', methods=['GET'])
+def get_favorites(user_id):
+    favorites = Favorite.query.filter_by(user_id=user_id).all()
+    return jsonify([favorite.serialize() for favorite in favorites]), 200
+
+
+@app.route('/favorites/<int:user_id>', methods=['POST'])
+def create_favorite(user_id):
+    body = request.get_json()
+    if body is None:
+        raise APIException("Request body is missing", status_code=400)
+    favorite = Favorite(**body, user_id=user_id)
+    db.session.add(favorite)
+    db.session.commit()
+    return jsonify({"msg": "Favorite created successfully"}), 201
+
+
+@app.route('/favorites/<int:user_id>', methods=['DELETE'])
+def delete_favorite(user_id):
+    body = request.get_json()
+    if body is None or "favorite_id" not in body:
+        raise APIException(
+            "Favorite ID is missing in the request body", status_code=400)
+    favorite_id = body["favorite_id"]
+    favorite = Favorite.query.get((user_id, favorite_id))
+    if favorite is None:
+        raise APIException("Favorite not found", status_code=404)
+    db.session.delete(favorite)
+    db.session.commit()
+    return jsonify({"msg": "Favorite deleted successfully"}), 200
+
+
+# CONTEXT
+
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
--- src/models.py
+++ src/models.py
@@ -2,18 +2,291 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+characters_in_films = db.Table('characters_in_films',
+                               db.Column('film_id', db.Integer, db.ForeignKey(
+                                   'films.uid'), primary_key=True),
+                               db.Column('person_id', db.Integer, db.ForeignKey(
+                                   'people.uid'), primary_key=True)
+                               )
+
+films_planets = db.Table('films_planets',
+                         db.Column('film_id', db.Integer, db.ForeignKey(
+                             'films.uid'), primary_key=True),
+                         db.Column('planet_id', db.Integer, db.ForeignKey(
+                             'planets.uid'), primary_key=True)
+                         )
+
+films_starships = db.Table('films_starships',
+                           db.Column('film_id', db.Integer, db.ForeignKey(
+                               'films.uid'), primary_key=True),
+                           db.Column('starship_id', db.Integer, db.ForeignKey(
+                               'starships.uid'), primary_key=True)
+                           )
+
+films_vehicles = db.Table('films_vehicles',
+                          db.Column('film_id', db.Integer, db.ForeignKey(
+                              'films.uid'), primary_key=True),
+                          db.Column('vehicle_id', db.Integer, db.ForeignKey(
+                              'vehicles.uid'), primary_key=True)
+                          )
+
+films_species = db.Table('films_species',
+                         db.Column('film_id', db.Integer, db.ForeignKey(
+                             'films.uid'), primary_key=True),
+                         db.Column('species_id', db.Integer, db.ForeignKey(
+                             'species.uid'), primary_key=True)
+                         )
+
+starship_pilots = db.Table('starship_pilots',
+                           db.Column('starship_id', db.Integer, db.ForeignKey(
+                               'starships.uid'), primary_key=True),
+                           db.Column('person_id', db.Integer, db.ForeignKey(
+                               'people.uid'), primary_key=True)
+                           )
+
+vehicle_pilots = db.Table('vehicle_pilots',
+                          db.Column('vehicle_id', db.Integer, db.ForeignKey(
+                              'vehicles.uid'), primary_key=True),
+                          db.Column('person_id', db.Integer, db.ForeignKey(
+                              'people.uid'), primary_key=True)
+                          )
+
+species_people = db.Table('species_people',
+                          db.Column('species_id', db.Integer, db.ForeignKey(
+                              'species.uid'), primary_key=True),
+                          db.Column('person_id', db.Integer, db.ForeignKey(
+                              'people.uid'), primary_key=True)
+                          )
+
+
 class User(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    is_active = db.Column(db.Boolean(), nullable=False)
+    favorites = db.relationship('Favorite', back_populates='user')
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return 'Username:{}, Favorites:{}'.format(self.username, self.favorites)
 
     def serialize(self):
         return {
+            "username": self.username,
             "id": self.id,
+            "favorites": [favorite.serialize() for favorite in self.favorites],
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "is_active": self.is_active,
+        }
+
+
+class Planet(db.Model):
+    __tablename__ = 'planets'
+    uid = db.Column(db.Integer, primary_key=True)
+    diameter = db.Column(db.String)
+    rotation_period = db.Column(db.String)
+    population = db.Column(db.String)
+    terrain = db.Column(db.String)
+    name = db.Column(db.String)
+
+
+    films = db.relationship(
+        'Film', secondary=films_planets, back_populates='planets')
+    favorites = db.relationship('Favorite', back_populates='planet')
+
+    def __repr__(self):
+        return '{}'.format(self.name)
+
+    def serialize(self):
+        return {
+            "name": self.name,
+            "diameter": self.diameter,
+            "rotation_period": self.rotation_period,
+            "population": self.population,
+            "terrain": self.terrain
+        }
+
+
+class Person(db.Model):
+    __tablename__ = 'people'
+    uid = db.Column(db.Integer, primary_key=True)
+    height = db.Column(db.String)
+    mass = db.Column(db.String)
+    hair_color = db.Column(db.String)
+    skin_color = db.Column(db.String)
+    birth_year = db.Column(db.String)
+    gender = db.Column(db.String)
+    name = db.Column(db.String)
+
+  
+    vehicles = db.relationship(
+        'Vehicle', secondary=vehicle_pilots, back_populates='pilots')
+    species = db.relationship(
+        'Species', secondary=species_people, back_populates='characters')
+    starships = db.relationship(
+        'Starship', secondary=starship_pilots, back_populates='pilots')
+    films = db.relationship(
+        'Film', secondary=characters_in_films, back_populates='characters')
+    favorites = db.relationship('Favorite', back_populates='person')
+
+    def __repr__(self):
+        return '{}'.format(self.name)
+
+    def serialize(self):
+        return {
+            "name": self.name,
+            "height": self.height,
+            "mass": self.mass,
+            "hair_color": self.hair_color,
+            "skin_color": self.skin_color,
+            "birth_year": self.birth_year,
+            "starship": [starship.name for starship in self.starships],
+            "films": [film.title for film in self.films],
+        }
+
+
+class Film(db.Model):
+    __tablename__ = 'films'
+    uid = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    episode_id = db.Column(db.Integer)
+    director = db.Column(db.String)
+    opening_crawl = db.Column(db.String)
+
+  
+    characters = db.relationship(
+        'Person', secondary=characters_in_films, back_populates='films')
+    planets = db.relationship(
+        'Planet', secondary=films_planets, back_populates='films')
+    starships = db.relationship(
+        'Starship', secondary=films_starships, back_populates='films')
+    vehicles = db.relationship(
+        'Vehicle', secondary=films_vehicles, back_populates='films')
+    species = db.relationship(
+        'Species', secondary=films_species, back_populates='films')
+    favorites = db.relationship('Favorite', back_populates='film')
+
+    def __repr__(self):
+        return '{}'.format(self.title)
+
+    def serialize(self):
+        return {
+            "title": self.title,
+        }
+
+
+class Starship(db.Model):
+    __tablename__ = 'starships'
+    uid = db.Column(db.Integer, primary_key=True)
+    films = db.relationship(
+        'Film', secondary=films_starships, back_populates='starships')
+    name = db.Column(db.String)
+    model = db.Column(db.String)
+    vehicle_class = db.Column(db.String)
+    manufacturer = db.Column(db.String)
+    cost_in_credits = db.Column(db.String)
+    pilots = db.relationship(
+        'Person', secondary=starship_pilots, back_populates='starships')
+    favorites = db.relationship('Favorite', back_populates='starship')
+
+    def __repr__(self):
+        return '{}'.format(self.name)
+
+    def serialize(self):
+        return {
+            "name": self.name,
+        }
+
+
+class Vehicle(db.Model):
+    __tablename__ = 'vehicles'
+    uid = db.Column(db.Integer, primary_key=True, nullable=True)
+    name = db.Column(db.String)
+    model = db.Column(db.String)
+    vehicle_class = db.Column(db.String)
+    manufacturer = db.Column(db.String)
+    cost_in_credits = db.Column(db.String)
+    films = db.relationship(
+        'Film', secondary=films_vehicles, back_populates='vehicles')
+    pilots = db.relationship(
+        'Person', secondary=vehicle_pilots, back_populates='vehicles')
+    favorites = db.relationship('Favorite', back_populates='vehicle')
+
+    def __repr__(self):
+        return '{}'.format(self.name)
+
+    def serialize(self):
+        return {
+            "name": self.name,
+        }
+
+
+class Species(db.Model):
+    __tablename__ = 'species'
+    uid = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    classification = db.Column(db.String)
+    designation = db.Column(db.String)
+    average_height = db.Column(db.String)
+    average_lifespan = db.Column(db.String)
+    hair_colors = db.Column(db.String)
+
+  
+    films = db.relationship(
+        'Film', secondary=films_species, back_populates='species')
+   
+    characters = db.relationship(
+        'Person', secondary=species_people, back_populates='species')
+    favorites = db.relationship('Favorite', back_populates='species')
+
+    def __repr__(self):
+        return '{}'.format(self.name)
+
+    def serialize(self):
+        return {
+            "name": self.name,
+        }
+
+
+class Favorite(db.Model):
+    __tablename__ = 'favorites'
+    user_id = db.Column(db.Integer, db.ForeignKey(
+        'users.id'), nullable=True)
+    film_id = db.Column(db.Integer, db.ForeignKey(
+        'films.uid'), nullable=True)
+    planet_id = db.Column(db.Integer, db.ForeignKey(
+        'planets.uid'), nullable=True)
+    person_id = db.Column(db.Integer, db.ForeignKey(
+        'people.uid'), nullable=True)
+    starship_id = db.Column(db.Integer, db.ForeignKey(
+        'starships.uid'), nullable=True)
+    vehicle_id = db.Column(db.Integer, db.ForeignKey(
+        'vehicles.uid'), nullable=True)
+    specie_id = db.Column(db.Integer, db.ForeignKey(
+        'species.uid'), nullable=True)
+    user = db.relationship('User', back_populates='favorites')
+    film = db.relationship('Film', back_populates='favorites')
+    planet = db.relationship('Planet', back_populates='favorites')
+    person = db.relationship('Person', back_populates='favorites')
+    starship = db.relationship('Starship', back_populates='favorites')
+    vehicle = db.relationship('Vehicle', back_populates='favorites')
+    species = db.relationship('Species', back_populates='favorites')
+    __table_args__ = (
+        db.PrimaryKeyConstraint('user_id', 'film_id', 'planet_id',
+                                'person_id', 'starship_id', 'vehicle_id', 'specie_id'),
+    )
+
+    def __repr__(self):
+        return 'Planets: {}, Film: {}, People: {}'.format(self.planet_id, self.film_id, self.person_id)
+
+    def serialize(self):
+        return {
+            "user_id": self.user_id,
+            "film_id": self.film_id,
+            "planet_id": self.planet_id,
+            "person_id": self.person_id,
+            "starship_id": self.starship_id,
+            "vehicle_id": self.vehicle_id,
+            "specie_id": self.specie_id
         }
